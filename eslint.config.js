17b5751bc01a4692167e96bd786029ff189b4943
alissonpef <alissonpef@gmail.com>
2026-07-14 14:09:46 -0300

    chore: configure formatting, linting, build tools, and license

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**', 'scripts/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier,
+];
